cd60435502d5c2b9fea9871b2872277b11d1cf39
Trent Hornibrook <trent.hornibrook@bjss.com>
2024-11-01 14:36:50 +1100

    updating providers

--- tf/providers.tf
+++ tf/providers.tf
@@ -3,7 +3,11 @@ provider "azurerm" {
   features {}
   # Credentials are automatically obtained via Azure CLI or environment variables
 }
-provider "fabric" {}
+provider "fabric" {
+  client_id = "9c3dd97d-66f0-4a9a-bdcf-514e63fbecb7"
+  tenant_id = "f7145ec9-2466-4aed-8a48-9b4a1813be6c"
+  oidc = true
+}
 # Data source to get information about the current authenticated session
 data "azurerm_client_config" "current" {}
 
