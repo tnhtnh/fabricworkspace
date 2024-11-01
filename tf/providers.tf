f9ad5108b366685e8055ca1a76a25f1e3297205b
Trent Hornibrook <trent.hornibrook@bjss.com>
2024-11-01 14:53:03 +1100

    more

--- tf/providers.tf
+++ tf/providers.tf
@@ -5,7 +5,15 @@ provider "azurerm" {
 }
 provider "fabric" {
   use_oidc = true
+  client_id = var.FABRIC_CLIENT_ID
+  tenant_id = var.FABRIC_TENANT_ID
 }
 # Data source to get information about the current authenticated session
 data "azurerm_client_config" "current" {}
 
+variable "FABRIC_CLIENT_ID" {
+  type = string
+}
+variable "FABRIC_TENANT_ID" {
+  type = string
+}
